7e4dc89cb48f41ed73c29871b64d4612d4529a29
marcelo mikosz <marcelo.mikosz@pavi.com.br>
2021-03-03 11:40:00 -0300

    Controller de moeda completa com os dois metodos de get e post

---
+++ MoedaApi/Controllers/MoedaController.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.Logging;
+using MoedaApi.DTO;
+using MoedaApi.Entidade;
+using Newtonsoft.Json;
+
+namespace MoedaApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MoedaController : ControllerBase
+    {
+        private readonly ILogger<MoedaController> _logger;
+        private List<MoedaDTO> _fila;
+        readonly IMemoryCache _cache;
+
+        public MoedaController(ILogger<MoedaController> logger, IMemoryCache memoryCache)
+        {
+            _logger = logger;
+            _cache = memoryCache;
+            _fila = new List<MoedaDTO>();
+        }
+
+        private string GetLastItemInserted()
+        {
+            string json;
+            int count = 0;
+            while (_cache.TryGetValue(count.ToString(), out json))
+            {
+                count++;
+            }
+            return json;
+        }
+
+        private int GetLastKeyInserted()
+        {
+            string json;
+            int count = 0;
+            while (_cache.TryGetValue(count.ToString(), out json))
+            {
+                count++;
+            }
+            return count;
+        }
+
+        [HttpPost]
+        public ActionResult<bool> AddItemFila(List<MoedaDTO> _dto)
+        {
+            // find last inserted
+            int lastkeyInserted = GetLastKeyInserted();
+            // set cache for 300 days
+            var cacheEntryOptions = new MemoryCacheEntryOptions()
+            .SetSlidingExpiration(TimeSpan.FromDays(300));
+            // Save data in cache.
+            _cache.Set(lastkeyInserted.ToString(), JsonConvert.SerializeObject(_dto), cacheEntryOptions);
+            _fila.AddRange(_dto);
+            return Ok(_fila);
+        }
+
+        [HttpGet]
+        public ActionResult<bool> GetItemFila()
+        {
+            // get the position in cache
+            int lastkeyInserted = GetLastKeyInserted() - 1;
+            string json;
+            // rescue the last item added
+            if (_cache.TryGetValue(lastkeyInserted.ToString(), out json))
+            {
+                // remove from cache
+                _cache.Remove(lastkeyInserted.ToString());
+                return Ok(JsonConvert.DeserializeObject<List<MoedaDTO>>(json));
+            }
+            else
+            {
+                return Unauthorized(new { msg = "erro" });
+            }
+
+        }
+    }
+}
---
+++ MoedaApi/DTO/MoedaDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MoedaApi.DTO
+{
+    public class MoedaDTO
+    {
+        public string Moeda { get; set; }
+        public DateTime Data_inicio { get; set; }
+        public DateTime Data_fim { get; set; }
+    }
+}
